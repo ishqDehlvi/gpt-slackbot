5273a99a85b060db5770eaec19aa503dd8d1f8a7
Inzemam ul-Haq <Inzemamhaq05@gmail.com>
2023-04-06 18:01:59 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+import re
+import time
+from threading import Thread
+
+from revChatGPT.revChatGPT import Chatbot
+from slack_bolt import App
+
+ChatGPTConfig = {
+        "email": os.environ['CHATGPT_EMAIL'],
+        "password": os.environ['CHATGPT_PASSWORD']
+    }
+
+app = App()
+chatbot = Chatbot(ChatGPTConfig, conversation_id=None)
+
+# Listen for an event from the Events API
+@app.event("app_mention")
+def event_test(event, say):
+    prompt = re.sub('(?:\s)<@[^, ]*|(?:^)<@[^, ]*', '', event['text'])
+    try:
+        response = chatbot.get_chat_response(prompt)
+        user = event['user']
+        user = f"<@{user}> you asked:"
+        asked = ['>',prompt]
+        asked = "".join(asked)
+        send = [user,asked,response["message"]]
+        send = "\n".join(send)
+    except Exception as e:
+        send = "We're experiencing exceptionally high demand. Please, try again."
+    say(send)
+
+def chatgpt_refresh():
+    while True:
+        chatbot.refresh_session()
+        time.sleep(60)
+
+if __name__ == "__main__":
+    thread = Thread(target=chatgpt_refresh)
+    thread.start()
+    app.start(4000)  # POST http://localhost:4000/slack/events
+    
